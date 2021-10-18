9fe0d011d0480bf49009ad51562c0690da2f2887
popam <popa.mariusz@gmail.com>
2021-10-18 22:02:50 +0200

    warsztaty

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 # Warsztaty Flask
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def moja_funkcja():
+    return 2 + 3
+
+@app.route("/", methods=["GET", "POST"])
+def hello_world():
+    name = request.form.get('name')
+    context = {
+        "name": name,
+        "city": moja_funkcja()
+    }
+    return render_template("index.html", context=context)
+
+
+@app.route("/history/<index_start>/<index_stop>")
+def history(index_start, index_stop):
+    context = {
+        "name": "Adam",
+        "start": index_start,
+        "stop": index_stop
+    }
+    return render_template("history.html", context=context)
 
